bb406c147e691746bdc058c3291ecfa6d30fc84d
DianisUribe <dianamariauribehernandez55@gmail.com>
2024-05-22 10:17:19 -0600

    Create acceso a una atraccion.py

---
+++ acceso a una atraccion.py
@@ -0,0 +1,7 @@
+altura=int(input("ingresa tu altura en cm: "))
+edad=int(input("introduce tu edad: "))
+
+if edad>=12 and altura >=120:
+        print("puedes subir a la atraccion")
+else:
+        print("no puedes subir a la atraccion")
