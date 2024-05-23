94bdfb6222fdb0a0945bc9e5c23dc230db3b3bce
DianisUribe <dianamariauribehernandez55@gmail.com>
2024-05-22 18:39:17 -0600

    Create deciciones multiples.py

---
+++ deciciones multiples.py
@@ -0,0 +1,9 @@
+calificacion=85
+if calificacion>=90:
+    print("exelente :)")
+elif calificacion>=70:
+    print("bueno")
+elif calificacion>=50:
+    print("suficiente")
+else:
+    print("insuficiente")
