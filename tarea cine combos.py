e964956d36b88d5dc8c93539d33945ff614a8c3a
DianisUribe <dianamariauribehernandez55@gmail.com>
2024-05-22 18:52:59 -0600

    Create tarea cine combos.py

---
+++ tarea cine combos.py
@@ -0,0 +1,18 @@
+print("bienvenidos al cine Dianamex jojojojo")
+dia=input("ingresa el dia:")
+if dia == 'lunes':
+    print("hoy es 2 X 1 jsjs")
+elif dia == 'martes':
+    print("hoy toca combo de $50")
+elif dia == 'miercoles':
+    print("refresco gratis en la compra de tus palomitas")
+elif dia == 'jueves':
+    print("refilm gratis ")
+elif dia == 'viernes':
+    print("no hay promo")
+elif dia == 'sabado':
+    print("entrada gratis en la compra de tu combo")
+elif dia == 'domingo':
+    print("no hay promos")
+else:
+    print("no existe")
