081d5185d549a97a990725089c499bec8a7d8c71
DianisUribe <dianamariauribehernandez55@gmail.com>
2024-05-22 18:51:08 -0600

    Create recomendacion de ropa segun el clima.py

---
+++ recomendacion de ropa segun el clima.py
@@ -0,0 +1,9 @@
+clima= float (input("ingresa la temperatura en grados °C: "))
+if clima <= 0:
+    print("busca tu cobijita peluda :)")
+elif clima <= 10:
+    print("usa ropa abrigadora")
+elif clima <= 20:
+    print("usa ropa ligera")
+else:
+    print("ropa muy ligera")
